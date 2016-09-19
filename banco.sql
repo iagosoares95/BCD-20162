f51c2bdc8cef72713f09802c95d393ac5306f69c
iagosoares95 <iagosoaressantos@hotmail.com>
2016-09-19 08:13:19 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,30 +1,39 @@
 create table aluno(
+  id unsigned int auto_increment primary key,
   nome varchar(128) not null,
-  documento char(11) primary key,
+  documento char(11),
   telefone unsigned bigint,
   email varchar(128)
 );
 
 create table curso(
-  numero smallint primary key,
+  id unsigned smallint auto_increment primary key,
+  numero smallint,
   nome varchar(128) not null,
   campus varchar(32) not null,
   chminima unsigned smallint,
   chmaxima unsigned smallint,
-  constraint fk_curso_campus_campus_nome
-  foreign key (campus) references campus(nome) 
+  curriculo varchar(64),
+  constraint fk_curso_campus_campus_id
+  foreign key (campus) references campus(id),
+  constraint fk_curso_curriculo_curriculo_implantacao
+  foreign key (curriculo) references curriculo(implantacao),
+  constraint fk_curso_curriculo_curriculo_disciplina 
+  foreign key(curriculo) references curriculo(disciplina)
 );
 
 create table campus(
-  nome varchar(128) primary key
+  id unsigned tinyint auto_increment primary key,
+  nome varchar(128)
 );
 
 create table matricula(
+  id unsigned int auto_increment primary key,
   aluno char(11),
   curso unsigned tinyint  not null,
-  numero char(10) primary key,
-  constraint fk_matricula_aluno_aluno_documento
-  foreign key (aluno) references aluno(documento),
-  constraint fk_matricula_curso_curso_numero
-  foreign key (curso) references curso(numero)
+  numero char(10),
+  constraint fk_matricula_aluno_aluno_id
+  foreign key (aluno) references aluno(id),
+  constraint fk_matricula_curso_curso_id
+  foreign key (curso) references curso(id)
 );
