10a01d890c20483881b10c32862a524a649e64b8
iagosoares95 <iagosoaressantos@hotmail.com>
2016-09-19 07:54:07 -0300

    Update and rename sql to banco.sql

--- banco.sql
+++ banco.sql
@@ -1,7 +1,7 @@
 create table aluno(
   nome varchar(128) not null,
   documento char(11) primary key,
-  telefone bigint,
+  telefone unsigned bigint,
   email varchar(128)
 );
 
@@ -9,8 +9,8 @@ create table curso(
   numero tinyint primary key,
   nome varchar(128) not null,
   campus varchar(32) not null,
-  chminima smallint,
-  chmaxima smallint,
+  chminima unsigned smallint,
+  chmaxima unsigned smallint,
   constraint fk_curso_campus_campus_nome,
   foreign key (campus) references campus(nome) 
 );
@@ -21,7 +21,7 @@ create table campus(
 
 create table matricula(
   aluno char(11),
-  curso tinyint  not null,
+  curso unsigned tinyint  not null,
   numero char(10) primary key,
   constraint fk_matricula_aluno_aluno_documento,
   foreign key (aluno) references aluno(documento)
